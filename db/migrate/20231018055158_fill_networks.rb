92a0d375858cf8c334899e648a66e865e2eb98a6
Aki Wu <wuminzhe@gmail.com>
2023-10-18 14:05:00 +0800

    fill networks

---
+++ db/migrate/20231018055158_fill_networks.rb
@@ -0,0 +1,21 @@
+class FillNetworks < ActiveRecord::Migration[7.1]
+  def up
+    require 'open-uri'
+    require 'json'
+
+    networks = JSON.parse(URI.open('https://chainid.network/chains_mini.json').read)
+    networks.each do |network|
+      Pug::Network.create(
+        chain_id: network['chainId'],
+        name: network['shortName'].underscore,
+        display_name: network['name'],
+        rpc_list: network['rpc'],
+        scan_span: 1000
+      )
+    end
+  end
+
+  def down
+    Pug::Network.delete_all
+  end
+end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_18_054931) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_18_055158) do
   create_table "pug_networks", force: :cascade do |t|
     t.integer "chain_id"
     t.string "name"
