d849fdbf7657af140254fbe8b29c0effddd7ebc8
Aki Wu <wuminzhe@gmail.com>
2023-10-30 17:35:39 +0800

    change network rpc and add explorer

--- db/migrate/20231018054931_create_pug_networks.rb
+++ db/migrate/20231018054931_create_pug_networks.rb
@@ -5,6 +5,7 @@ class CreatePugNetworks < ActiveRecord::Migration[7.1]
       t.string :name
       t.string :display_name
       t.string :rpc
+      t.string :explorer
       t.integer :scan_span, default: 5000
       t.integer :last_scanned_block, default: 0
 
--- db/migrate/20231018055158_fill_pug_networks.rb
+++ db/migrate/20231018055158_fill_pug_networks.rb
@@ -2,14 +2,17 @@ class FillPugNetworks < ActiveRecord::Migration[7.1]
   def up
     require 'open-uri'
 
-    networks = JSON.parse(URI.open('https://chainid.network/chains_mini.json').read)
+    networks = JSON.parse(URI.open('https://chainid.network/chains.json').read)
     ActiveRecord::Base.transaction do
       networks.each do |network|
+        rpc = network['rpc']&.select { |url| url.start_with?('http') && url !~ /\$\{(.+)\}/ }&.first
+        explorer = network['explorers']&.first&.[]('url')
         Pug::Network.create(
           chain_id: network['chainId'],
           name: network['shortName'].underscore,
           display_name: network['name'],
-          rpc: Pug.filter_rpc_list(network['rpc'])&.first,
+          rpc:,
+          explorer:,
           scan_span: 5000
         )
       end
