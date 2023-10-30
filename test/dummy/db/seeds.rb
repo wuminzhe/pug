a1f97aeb5d98c69e47be52c743ca82382d9696fc
Aki Wu <wuminzhe@gmail.com>
2023-10-30 16:15:37 +0800

    update seed

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -2,8 +2,8 @@ puts '== Start seeding'
 puts `rails 'app:pug:add_contract[421614,0x0034607daf9c1dc6628f6e09e81bb232b6603a89]'`
 puts `rails 'app:pug:add_contract[421614,0x007eed6207c9af3715964fb7f8b5f44e002a3498]'`
 puts `rails 'app:pug:add_contract[421614,0x0002396f1d52323fcd1ae8079b38808f046882c3]'`
-puts `rails 'app:pug:add_contract[421614,0x001F4321429B1E2DF774bdAe0fc62A7394652E0F]'`
+puts `rails 'app:pug:add_contract[421614,0x00945C032A37454333d7044a52a5A42Aa0f6c608]'`
 puts `rails 'app:pug:add_contract[44,0x0034607daf9c1dc6628f6e09e81bb232b6603a89]'`
 puts `rails 'app:pug:add_contract[44,0x007eed6207c9af3715964fb7f8b5f44e002a3498]'`
 puts `rails 'app:pug:add_contract[44,0x0002396f1d52323fcd1ae8079b38808f046882c3]'`
-puts `rails 'app:pug:add_contract[44,0x001F4321429B1E2DF774bdAe0fc62A7394652E0F]'`
+puts `rails 'app:pug:add_contract[44,0x00945C032A37454333d7044a52a5A42Aa0f6c608]'`
